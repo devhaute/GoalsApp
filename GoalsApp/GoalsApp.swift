b86cb2aded9327f28cc74a64a63f6aa69badcec7
devhaute <devhaute@gmail.com>
2022-01-19 20:26:28 +0900

    디렉터리 구조 세팅

--- GoalsApp/GoalsApp.swift
+++ GoalsApp/GoalsApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Firebase
 
 @main
 struct GoalsApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
