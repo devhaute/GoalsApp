d3f0cdba9e4a1fc1d4aa67ccbccf0084cba06284
devhaute <devhaute@gmail.com>
2022-01-19 19:17:11 +0900

    프로젝트명 변경

--- GoalsApp/ContentView.swift
+++ GoalsApp/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  TinderClone
-//
-//  Created by devhaute on 2022/01/15.
-//
-
 import SwiftUI
 
 struct ContentView: View {
---
+++ GoalsApp/GoalsApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct GoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
